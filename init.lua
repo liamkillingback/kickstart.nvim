65cd75afb84d5d52a1e28175421fecd8a621242e
Liam Killingback <liam_killingback@yahoo.com>
2026-05-10 20:41:19 +1000

    Add elixirls configuration to init.lua

--- init.lua
+++ init.lua
@@ -704,6 +704,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+        elixirls = {},
         zls = {
           -- cmd = { ... },
           -- filetypes = { ... },
